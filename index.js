f4dcab95d278ee0d36a4a6e3cdb296e0a3b3e187
Nathaniel Ezeanaka <63201671+nathphil9411@users.noreply.github.com>
2024-05-04 18:24:54 -0400

    Update index.js
    Add comment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // calculate the are of triangle
 // 1/2 bh 
+// b and h are parameters 
 const calculateArea = (b,h)=>{
     const area = 0.5*b*h
     console.log(`The area is ${area} sqm`)
 }
 
+// 2, 3 are arguments 
 calculateArea(2,3)
