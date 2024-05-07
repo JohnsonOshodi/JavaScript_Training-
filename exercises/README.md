f93c20df8ce424bea01967befc38ba5564c8fe66
Ezeanaka Nathaniel Nwabueze <ezeanakanath@gmail.com>
2024-05-07 09:18:52 +0100

    Revert "Add exercise 1 and the  instructions"
    This reverts commit c6ec5fee971d3464e7e30878a5d772f1d1da978e.

---
+++ exercises/README.md
@@ -0,0 +1,24 @@
+# Instructions
+
+Exercises will reinenforce what your are learning from the tutorials and the dates of completion are actually very important.
+Please always make the best effort in tackling these challenges.
+
+## Structure
+
+- Run the command git pull to get updated assignment folder, once an exercise is given.
+- Create a folder called Exercises on the root project folder, this folder would house all exercises.
+- Create a javaScript file for each exercise according to the exercice name; example `exercise1.2.js`
+
+## Timing
+
+- All exercises should be carried out within the stipulated time located at the top rigth hand corner of each assignment.
+- It is very important to stick to time to avaoid code concept non-reinforcement and overcome coding phobia.
+
+## Ethics
+
+- Try as much as possible to avoid generating code blocks using generative AIs such as cluade and gpts, they kill creativity of beginners and intermediate learners. Only use them for debuging.
+- Don't clone a ready made repository, try as much as possible to solve the problem by yourself. Use google, stackoverflow and others.
+
+## Tips
+
+While you solve these problems develop a problem solving framwork that help you the most in achieving success. Document the process and repeat. Happy coding.
--- order-summary/order-summary.js
+++ order-summary/order-summary.js
@@ -1,28 +1,28 @@
 //order summary using concatenation
 
-"Items (" + (1 + 1) + "): $" + (2095 + 799) / 100;
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
 
-console.log("Items (" + (1 + 1) + "): $" + (2095 + 799) / 100);
+console.log('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100,);
 
 //function for shipping & handling
 function getshippingAndHandling() {
-	return "Shipping & handling: $0.00";
+    return "Shipping & handling: $0.00";
 }
 const shippingAndHandling = getshippingAndHandling();
 console.log(shippingAndHandling);
 
 // function for tax
 function getTotalBeforeTax() {
-	return "Total before tax:  $28.94";
+    return "Total before tax:  $28.94";
 }
-const totalBeforeTax = getTotalBeforeTax();
+const totalBeforeTax= getTotalBeforeTax();
 console.log(totalBeforeTax);
 
 // function for estimated tax
-function calculateEstimatedTax() {
-	const taxRate = 0.1;
-	const estimatedTax = totalAmount * taxRate;
-	return "Estimated tax (10%) : $" + estimatedTax.toFixed(2);
+function calculateEstimatedTax () {
+    const taxRate = 0.10;
+    const estimatedTax = totalAmount * taxRate;
+    return "Estimated tax (10%) : $" + estimatedTax.toFixed(2);
 }
 
 const totalAmount = 2.89;
@@ -33,22 +33,24 @@ console.log(estimatedTax);
 // function for total order
 
 function calculateOrderTotal(subtotal, tax) {
-	const orderTotal = subtotal + tax;
-	return "Order total: $" + orderTotal.toFixed(2);
+    const orderTotal = subtotal + tax;
+    return "Order total: $" + orderTotal.toFixed(2);
 }
 
 const subtotal = 28.94;
-const tax = 2.89;
+const tax = 2.89
 const orderTotal = calculateOrderTotal(subtotal, tax);
 console.log(orderTotal);
 
 // function to display use paypal
 function usePayPal() {
-	return "Use PayPal";
+    return "Use PayPal";
 }
 
 console.log(usePayPal());
-// this is good, keep pushing.
+
+
+
 
 // order summary using interpolation
 // `Items (${1 + 1}): $${(2095 + 799) / 100}`
