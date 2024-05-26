f7d51eeb8a1230ff24f1c777088fa95e06f3dd8f
Johnson-Oshodi <johnsonoshodi2@gmai.com>
2024-05-26 11:00:31 -0600

    addStringMonthDay

--- exercises/exercise-solutions/exercise3-solution.js
+++ exercises/exercise-solutions/exercise3-solution.js
@@ -6,13 +6,12 @@ const currentDate = new Date();
 // get different parts of the date
 const year = currentDate.getFullYear();
 
-const month = currentDate.getMonth() + 1; 
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
 
-const day = currentDate.getDate();
+const day = String(currentDate.getDate()).padStart(2, '0');
 
 
 // format the date as a string
-
 const formattedDate = `${year}-${month}-${day}`;
 
 console.log(`current Date: ${formattedDate}`);
