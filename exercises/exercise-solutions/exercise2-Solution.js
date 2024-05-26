061f7a7522bc6da19b0c69091a3aa92c56e077f1
Johnson-Oshodi <johnsonoshodi2@gmai.com>
2024-05-26 05:12:30 -0600

    *newExercise2bAdded*

--- exercises/exercise-solutions/exercise2-Solution.js
+++ exercises/exercise-solutions/exercise2-Solution.js
@@ -1,40 +1,60 @@
 //Write a JavaScript program to display the current day and time in the following format.
 
 //  current date and time
-function displayCurrentDayAndTime() {
-  const today = new Date();
+// function displayCurrentDayAndTime() {
+//   const today = new Date();
 
-  // weekday names
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//   // weekday names
+//   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   
 
-  // current day of the week
-  const dayName = daysOfWeek[today.getDay()];
+//   // current day of the week
+//   const dayName = daysOfWeek[today.getDay()];
 
-  // current time components
-  let hour = today.getHours();
-  const minutes = today.getMinutes();
-  const seconds = today.getSeconds();
+//   // current time components
+//   let hour = today.getHours();
+//   const minutes = today.getMinutes();
+//   const seconds = today.getSeconds();
 
-  // AM or PM
-  const amOrPm = hour >= 12 ? 'PM' : 'AM';
+//   // AM or PM
+//   const amOrPm = hour >= 12 ? 'PM' : 'AM';
   
-  // convert hours from 24-hour format to 12-hour format
+//   // convert hours from 24-hour format to 12-hour format
 
-  hour = hour % 12;
-  hour = hour ? hour : 12;
+//   hour = hour % 12;
+//   hour = hour ? hour : 12;
 
-  // minutes and seconds to always have two digits
+//   // minutes and seconds to always have two digits
 
-  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-  const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+//   const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+//   const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
 
-  // output
+//   // output
 
-  console.log(`Today is : ${dayName}.`);
-  console.log(`current time is : ${hour} ${amOrPm} : ${formattedMinutes} : ${formattedSeconds}`);
+//   console.log(`Today is : ${dayName}.`);
+//   console.log(`current time is : ${hour} ${amOrPm} : ${formattedMinutes} : ${formattedSeconds}`);
 
+// }
+
+
+// displayCurrentDayAndTime();
+
+
+//Exercise 2B Solution
+function rotateStringRight(str) {
+   let charArray = str.split('');
+
+   function rotateOnce() {
+    let lastChar = charArray.pop();
+
+    charArray.unshift(lastChar);
+
+    console.log(charArray.join(''));
+   }
+
+   // rotate the string every 1 second
+   setInterval(rotateOnce, 1000);
 }
 
+rotateStringRight('w3resourse');
 
-displayCurrentDayAndTime();
